067862c302fca84d1d5ed60b052f09ee6c4e0f71
0woong <jooyy1219@naver.com>
2023-06-12 15:05:49 +0900

    modal update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,57 +6,54 @@ import Modal from "./modal/Modal";
 
 function Projects() {
   // 모달창 노출 여부 state
-  const [modalOpen, setModalOpen] = useState(false);
+  const [displayModal, setDisplayModal] = useState(false);
 
   // 모달창 노출
-  const showModal = () => {
-    setModalOpen(true);
-  };
+  const showModal = () => setDisplayModal(true);
 
   return (
     <>
       <section className="page-section projects-bg" id="projects">
         <h2>Projects</h2>
         <hr className="divider" />
-        <div className="container">
+        <div onClick={showModal} className="container">
           <div className="row">
-            <div className="projects-box" onClick={showModal}>
-              {modalOpen && <Modal setModalOpen={setModalOpen} />}
+            <div className="projects-box">
               <img className="thumbnails" src={projectImg} alt="..." />
               <div className="projects-box-desc">
                 <div className="project-skill">Next js</div>
                 <div className="project-name">Todo List</div>
               </div>
             </div>
-            <div className="projects-box" onClick={showModal}>
+            <div className="projects-box">
               <img className="thumbnails" src={projectImg} alt="..." />
               <div className="projects-box-desc">
                 <div className="project-skill"></div>
                 <div className="project-name"></div>
               </div>
             </div>
-            <div className="projects-box" onClick={showModal}>
+            <div className="projects-box">
               <img className="thumbnails" src={projectImg} alt="..." />
               <div className="projects-box-desc">
                 <div className="project-skill"></div>
                 <div className="project-name"></div>
               </div>
             </div>
-            <div className="projects-box" onClick={showModal}>
+            <div className="projects-box">
               <img className="thumbnails" src={projectImg} alt="..." />
               <div className="projects-box-desc">
                 <div className="project-skill"></div>
                 <div className="project-name"></div>
               </div>
             </div>
-            <div className="projects-box" onClick={showModal}>
+            <div className="projects-box">
               <img className="thumbnails" src={projectImg} alt="..." />
               <div className="projects-box-desc">
                 <div className="project-skill"></div>
                 <div className="project-name"></div>
               </div>
             </div>
-            <div className="projects-box" onClick={showModal}>
+            <div className="projects-box">
               <img className="thumbnails" src={projectImg} alt="..." />
               <div className="projects-box-desc">
                 <div className="project-skill"></div>
@@ -65,6 +62,7 @@ function Projects() {
             </div>
           </div>
         </div>
+        <Modal displayModal={displayModal} setDisplayModal={setDisplayModal}></Modal>
       </section>
       <section className="page-section projects-desc">
         <div className="">
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,17 +1,30 @@
+import { useEffect, useRef } from "react";
 import styles from "./Modal.module.css";
 
-function Modal({ setModalOpen, id, title, content, writer }) {
-  // 모달 끄기
-  const closeModal = () => {
-    setModalOpen(false);
+function Modal({ displayModal, children, setDisplayModal }) {
+  const wrapperRef = useRef();
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+  const handleClickOutside = (event) => {
+    if (wrapperRef && !wrapperRef.current.contains(event.target)) {
+      setDisplayModal(false);
+    }
   };
-
+  const closeModal = () => {
+    setDisplayModal(false);
+  };
   return (
-    <div className={styles.container}>
-      <button className={styles.close} onClick={closeModal}>
-        X
-      </button>
-      <p>모달창입니다.</p>
+    <div style={{ display: displayModal ? "block" : "none" }} className={styles.container}>
+      <div ref={wrapperRef} className={styles.modalBox}>
+        <button className={styles.close} onClick={closeModal}>
+          X
+        </button>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
